3be858c60c0a1e802a46ebb7aa373fd6c5fc25f0
GitHub Action <action@github.com>
2024-10-27 09:04:53 +0000

    Template cleanup

--- src/main/kotlin/com/github/bonevidy/json2tsnext/MyBundle.kt
+++ src/main/kotlin/com/github/bonevidy/json2tsnext/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.bonevidy.json2tsnext
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/bonevidy/json2tsnext/listeners/MyApplicationActivationListener.kt
+++ src/main/kotlin/com/github/bonevidy/json2tsnext/listeners/MyApplicationActivationListener.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.bonevidy.json2tsnext.listeners
 
 import com.intellij.openapi.application.ApplicationActivationListener
 import com.intellij.openapi.diagnostic.thisLogger
